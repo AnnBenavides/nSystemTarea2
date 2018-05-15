ab1db28c859a4754f66e8c8b4fe90c983b2efe01
AnnBenavides <andrea.benavidesj@gmail.com>
2018-05-14 20:54:40 -0400

    nExchange: enunciado

--- nsystem64-beta2/src/nExchange.c
+++ nsystem64-beta2/src/nExchange.c
@@ -1,1 +1,14 @@
+/**
+ENUNCIADO
+t1 -> nExchange(t2,m1,dt1)
+	t1 se bloquea hasta que
+		1. t2 llama nExchange(t1,m2,dt2)
+			-> t1 retorna m2
+			!   a. t2 retorna m1 inmediatamente
+				b. t2 espera bloqueado la llamada de t1 o el timeout de dt2
+		2. tiempo de espera supera a dt1
+			-> t1 retorna NULL
+		3. si dt1 = -1, t1 espera el nEchange de t2 indefinidamente
+**/
+
 void * nExchange(nTask t, void *msg, int timeout);
